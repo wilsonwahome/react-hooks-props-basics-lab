e2ad65a5164018ea71457d0baf2f1f12aebf3aa2
wahome <wahomewgitonga@gmail.com>
2024-05-09 00:04:34 +0300

    update

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({ bio }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio && <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github="https://github.com/liza" />
+      <Links linkedin="https://www.linkedin.com/in/liza/" />
     </div>
   );
 }
